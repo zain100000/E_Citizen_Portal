3a867f4f449c4a70ca5f414c89fa35dd5a3f11dd
zain100000 <muhammadzainulabideen292@gmail.com>
2023-11-07 13:11:37 +0500

    commit

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -3,13 +3,14 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA7m11CxjZ_PoY9F25uOk_WHp3xENfrwHQ',
-  authDomain: 'crime-report-32be4.firebaseapp.com',
-  databaseURL: 'https://crime-report-32be4-default-rtdb.firebaseio.com',
-  projectId: 'crime-report-32be4',
-  storageBucket: 'crime-report-32be4.appspot.com',
-  messagingSenderId: '238545625209',
-  appId: '1:238545625209:web:7e44c20cb94f14089774a8',
+  apiKey: 'AIzaSyDxT5fMvK5Ai9klPYEZElIy3kaHc4kd_mA',
+  authDomain: 'e-citizen-portal-d5230.firebaseapp.com',
+  databaseURL: 'https://e-citizen-portal-d5230-default-rtdb.firebaseio.com',
+  projectId: 'e-citizen-portal-d5230',
+  storageBucket: 'e-citizen-portal-d5230.appspot.com',
+  messagingSenderId: '187456587043',
+  appId: '1:187456587043:web:0e974c4f8cc70e7ab73896',
+  measurementId: 'G-HLE50EGMD1',
 };
 
 firebase.initializeApp(firebaseConfig);
